2e03f725b526d8d28c06df6d17c5a38c7dacdac2
vikram raut <vikram.raut8@gmail.com>
2021-05-28 17:02:02 +0530

    created code1

---
+++ code1.py
@@ -0,0 +1,9 @@
+def run():
+  print("if you are there please start runnning!")
+  read=input("please tell what to do")
+  if read == "run"
+    print("he is running ")
+  else: 
+    print("he is not even trying to run")
+    
+ 
